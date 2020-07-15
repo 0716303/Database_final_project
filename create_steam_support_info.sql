c712e8640c27ecde7fdbcd491964a10473fd3d90
0716303 <62974443+0716303@users.noreply.github.com>
2020-07-15 21:43:21 +0800

    Add files via upload

--- create_steam_support_info.sql
+++ create_steam_support_info.sql
@@ -1,14 +1,14 @@
 create table steam_support_info(
 	steam_appid int not NULL,
-    website varchar(500),
-    support_url varchar(500),
-    support_email varchar(500),
+    website TEXT,
+    support_url TEXT,
+    support_email TEXT,
     primary key (steam_appid)
     );
     
 load data local infile './steam_support_info.csv'
 into table steam_support_info
 fields terminated by ','
-enclosed by '"'
+OPTIONALLY enclosed by '"'
 lines terminated by '\n'
 ignore 1 lines;
